842d21d50d2f59991dd17b51cee342907a60edbe
Gustavo Carneiro Quinto <gutocross.kx@gmail.com>
2021-11-08 21:42:17 -0300

    Sistema delogin

---
+++ Login/login.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+require('C:\Users\Gustavo\Xampps\htdocs\eCoins\Banco\conectaBD.php');
+/*if (empty($_POST['email']) || empty($_POST['password'])){
+    echo '<script> Os dados fornecidos estão incorretos. </script>';
+    include('login.html');
+}*/
+$consulta = $bd -> prepare('SELECT
+                            nome, email, senha
+                            FROM
+                            usuarios
+                            WHERE
+                            email = :email');
+
+$consulta -> execute([':email' => $_POST['email']]);
+$retornoConsulta = $consulta -> fetch(PDO::FETCH_ASSOC);
+
+
+if($retornoConsulta && $_POST['email'] == $retornoConsulta['email'] && password_verify($_POST['password'], $retornoConsulta['senha'])){
+    $_SESSION['usuario'] = $retornoConsulta['nome'];
+    echo'<script> alert("Usuário autenticado") </script>';
+    include('menu.html');
+    
+}
+else{
+    echo'<script> alert("Dados incorretos") </script>';
+    include ('login.html');
+}
+                            
+                            
+
+
+
+
