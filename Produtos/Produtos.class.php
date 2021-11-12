8078d6da41f62532bd1574b4720d63039a4bfd9f
Gustavo Carneiro Quinto <gutocross.kx@gmail.com>
2021-11-12 19:18:03 -0300

    CRUD Produtos v1.0

--- Produtos/Produtos.class.php
+++ Produtos/Produtos.class.php
@@ -4,25 +4,77 @@
 class Product {
 
     function addProduct($bd, $name, $value, $quant, $file){
-        $add = $bd -> prepare("INSERT INTO
-                produtos (nomeproduto, valor, quantidade, imagem)
-                VALUES
-                (:nomeproduto, :valor, :quantidade, :imagem)");
+        $add = $bd -> prepare("INSERT INTO produtos (nomeproduto, valor, quantidade, imagem)
+                               VALUES (:nomeproduto, :valor, :quantidade, :imagem)");
         $values[':nomeproduto'] = $name;
-        $values[':valor'] = str_replace(",",".", $value);
+        $values[':valor'] = $value;
         $values[':quantidade'] = $quant;
         $values[':imagem'] = $file;
 
         $add -> execute($values);
     }
-    function attProduct(){
+    function attProduct($bd, $id, $nome, $valor, $quant){
+        if(empty($nome) || empty($valor) || empty($quant)){
+            echo "<script> alert('Dados preenchidos incorretamente, as alterações não foram salvas.')</script>";
+            exit();
+        }
+        $att = $bd -> prepare("UPDATE produtos
+                               SET nomeproduto = '{$nome}', valor = '{$valor}', quantidade = '{$quant}'
+                               WHERE id = {$id}");
+        $att -> execute();
+    }
+    function delProduct($bd, $produto){
+        $prepare = $bd -> prepare('DELETE FROM produtos
+                                   WHERE imagem = :imagem');
+                $prepare -> execute([':imagem' => $_POST['deletar']]);
+        unlink($produto);
+        $this->attPage();
+    }
+    function showProducts($bd){
+        $sql = 'SELECT id, nomeproduto, valor, quantidade, imagem
+                FROM produtos';
+        $query = $bd -> query($sql);
+        foreach($query as $registro){
+                echo"<div id='testando2'>
+                        <div id='testando3'>
+                            <form class='form' id='form' method='post' action='Produtos.php'>
+                                <h3> {$registro['nomeproduto']} </h3>
+                                <img src='{$registro['imagem']}'>
+                                <h3> Estoque: {$registro['quantidade']}</h3>
+                                <h3> Valor: R$ {$registro['valor']} </h3>
+                                <button type='submit' name='editar' value='{$registro['id']}'> Editar </button>
+                                <br>
+                                <button type='submit' name='deletar' value='{$registro['imagem']}'> Deletar </button>
+                                <hr></hr>
+                            </form>
+                        </div>
+                    </div>";
+        }
         
     }
-    function delProduct(){
-        
+    function queryProduct($bd, $id){
+        $sql = $bd -> prepare("SELECT nomeproduto, valor, quantidade, imagem
+                FROM produtos
+                WHERE id = {$id}");
+        $sql -> execute();
+        $consulta = $sql -> fetch(PDO::FETCH_ASSOC);
+        echo"<form class='form' method='post' action='Produtos.php'>
+                <h2> {$consulta['nomeproduto']} -- ID: {$id} </h2>
+                <img src='{$consulta['imagem']}'>
+                <br>
+                <label for='nome'> Nome </label>
+                <input type='text' name='nome' value='{$consulta['nomeproduto']}'>
+                <label for='valor'> Valor </label>
+                <input type='number' name='valor' value='{$consulta['valor']}'>
+                <label for='quant'> Quantidade </label>
+                <input type='number' name='quant' value='{$consulta['quantidade']}'>
+                <br>
+                <button type='submit' name='id' value='{$id}'> Aplicar alterações </button>
+            </form>";
+
     }
-    function showProduct(){
-        
+    function attPage() {
+        echo"<meta HTTP-EQUIV='refresh' CONTENT='0'>";
     }
 
 
--- Produtos/Produtos.php
+++ Produtos/Produtos.php
@@ -1,18 +1,36 @@
-<?php   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="produtos.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="testando">
+        <a style="font-size: 35px;" href="CadProdutos.html"> Cadastrar novo produto </a>
+        <?php
+            include('../Banco/conectaBD.php');
+            include('Produtos.class.php');
 
-session_start();
-include('Produtos.class.php');
-include('../Banco/conectaBD.php');
+            $obj = new Product();
 
-$obj = new Product;
-
-$filesTMP = $_FILES['file']['tmp_name'];
-$directory = 'Produtosimagem/';
-$newname = uniqid().'.jpeg';
-
-if(mime_content_type($filesTMP) == 'image/jpeg'){
-
-    move_uploaded_file($filesTMP, $directory.$newname);
-    $obj->addProduct($bd, $_POST['name'], $_POST['value'], $_POST['quant'], $directory.$newname);
-
-};
+            if (isset($_POST['deletar'])){
+                $obj-> delProduct($bd, $_POST['deletar']); 
+            }
+            if (isset($_POST['editar'])){
+                $obj -> queryProduct($bd, $_POST['editar']);
+            }
+            else{
+                $obj-> showProducts($bd);
+            }
+            if(isset($_POST['id'])){
+                $obj-> attProduct($bd, $_POST['id'], $_POST['nome'], $_POST['valor'], $_POST['quant']);
+                $obj->attPage();
+                unset($_POST);
+            }
+        ?>
+    </div>
+    </body>
+</html>
